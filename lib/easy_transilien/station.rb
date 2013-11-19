af3dbd502338855d23939ae85f6b52f846239008
Thomas Lecavelier <tl@pierlis.com>
2013-11-19 13:02:01 +0100

    Station.find return all Stations

--- lib/easy_transilien/station.rb
+++ lib/easy_transilien/station.rb
@@ -17,23 +17,29 @@ module EasyTransilien
       # Find a list of `Station` matching +criterium+.
       # Valid keys:
       # @param [String] criterium will try to match +name+ or +external_code+. Both case unsensitive.
-      def find(criterium)
-        stations = []
+      def find(criterium = nil)
         if criterium.is_a?(String)
           regex = /#{criterium}/i
           matching = all_stop_areas.reject { |sa| sa.name !~ regex && sa.external_code !~ regex }
-          stations = matching.map do |sa|  
-            item = new
-            item.name = sa.name
-            item.external_code = sa.external_code
-            item.access_time = sa.access_time
-            item
-          end
+          return convert_stop_areas_to_stations(matching)
         elsif criterium.is_a?(Station)
-          stations << criterium
+          return [criterium]
+        elsif criterium.nil?
+          return convert_stop_areas_to_stations(all_stop_areas)
         end
-        stations
       end
+
+      def convert_stop_areas_to_stations(stop_areas)
+        stop_areas.map do |sa|  
+          item = new
+          item.name = sa.name
+          item.external_code = sa.external_code
+          item.access_time = sa.access_time
+          item
+        end
+      end
+
     end
   end
+
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -9,4 +9,9 @@ describe EasyTransilien::Station do
     col.first.is_a?(EasyTransilien::Station).should be_true
     col.length.should eql(2)
   end
+  it 'should get all Station for empty search' do
+    col = EasyTransilien::Station.find
+    col.is_a?(Array).should be_true
+    (col.length > 0).should be_true
+  end
 end
