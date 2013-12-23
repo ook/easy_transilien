1a34e87a4bb25618761fafd595bec4a9fd6cecca
Thomas Lecavelier <tl@pierlis.com>
2013-12-23 22:33:18 +0100

    Reverse Line  finder system: we want them by commercial letter

--- lib/easy_transilien/line.rb
+++ lib/easy_transilien/line.rb
@@ -1,7 +1,7 @@
 module EasyTransilien
   # ET::Line is NOT the translation of MS::Line
   # It's the port of commercial names known by customers: "Ligne J", "Ligne L", etc.
-  # So, it's a collection of MS::Line, with a common MS::Network
+  # So, it's a collection of MS::Line, with (normally) a common MS::Network
   class Line
     attr_accessor :name, :access_time, :codes
     attr_accessor :ms_network
@@ -15,14 +15,27 @@ module EasyTransilien
         @all_networks
       end
 
-      # Find a line via a single letter (A,B,C,D,E,I,J,K,L
+      def all_ms_lines(options = {})
+        options[:force] ||= false
+        if options[:force] || @all_ms_lines.nil?
+          @all_ms_lines = ::Transilien::Line.find
+        end
+        @all_ms_lines
+      end
+
+      # Find a line via a single letter (A,B,C,D,E,I,J,K,L, etc.)
       def find(criterium = nil)
         if criterium.is_a?(String)
           regexp = /#{criterium}/i
-          matching = all_networks.reject { |n| n.name !~ regexp && n.external_code !~ regexp }
-          return convert_networks_to_lines(matching)
-        else
+          matching = all_networks.reject { |n| n.name !~ regexp && n.external_code !~ regexp && !n.codes.map(&:downcase).include?(criterium.downcase) }
+          if matching.length == 1 # Ok, seems sucessful
+            return convert_networks_to_lines(matching)
+          end
+        elsif criterium.nil?
+          # No Args? Return everything.
           return convert_networks_to_lines(all_networks)
+        else
+          raise "Can't understand #find criteria: not a letter, not nil, not a network line name: #{criterium.inspect}"
         end
       end
 
@@ -30,19 +43,20 @@ module EasyTransilien
         networks.map do |n|
           item = new
           item.name          = n.name
-          item.line_external_codes << n.external_code
           item.access_time   = n.access_time
           item.ms_network    = n
           item.ms_lines << n.lines
+          item.ms_lines.compact!
           item
         end
       end
     end # << self
 
     def line_external_codes
-      @lines_external_codes ||= []
-      @lines_external_codes.flatten!
-      @lines_external_codes
+      @lines_external_codes ||= begin
+        return nil unless ms_lines
+        ms_lines.map(&:external_code).sort.uniq
+      end
     end
 
     def codes
@@ -56,7 +70,7 @@ module EasyTransilien
     end
 
     def stations
-      @stations ||= ms_lines.map { |ms_line| ms_line.stop_areas }.uniq
+      @stations ||= EasyTransilien::Station.convert_stop_areas_to_stations(ms_lines.map { |ms_line| ms_line.stop_areas }.flatten.uniq)
     end
 
   end
